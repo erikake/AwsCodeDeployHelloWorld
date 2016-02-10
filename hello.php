c788a4562cdb677704389a4d2382af1c31eff970
erika <erika@192.168.0.113>
2016-02-10 14:45:08 +0100

    only php

--- hello.php
+++ hello.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title><?php echo "Hellow World"; ?></title>
-    </head>
-    <body>
-        <?php
+<?php
 phpinfo();
 ?>
-    </body>
-</html>
