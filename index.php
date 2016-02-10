bcf21020293f96bc6dca4f7cbbd798af0256a471
erika <erika@192.168.0.113>
2016-02-10 21:01:32 +0100

    changing root layout

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hello World</title>
+    </head>
+    <body>
+        A hellow world page 5
+        <?php echo "pgp enabled" ?>
+    </body>
+</html>
+
+
+
+<?php
+phpinfo();
+?>
